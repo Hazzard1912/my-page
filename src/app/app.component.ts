28a5b71292510857718cac983133bab3b311ac5b
Hazzard1912 <jhonnier.hernandez@correounivalle.edu.co>
2023-07-08 19:06:32 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import * as ScrollReveal from 'scrollreveal';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'portfolio';
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    ScrollReveal().reveal('.about');
+  }
 }
